4e3f8d489e627c70d85d690fa807ecfe049ff13a
Jocephuss <rowlandjosiah14@gmail.com>
2024-11-17 14:28:53 -0700

    "minot changes to deploy"

--- config/db.js
+++ config/db.js
@@ -2,16 +2,11 @@ const mongoose = require("mongoose");
 
 const connect = async () => {
   try {
-    const mongoURI =
-      process.env.MONGO_URI || "mongodb://localhost:27017/flashcards";
-    await mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Connected to MongoDB");
-  } catch (err) {
-    console.error("Error connecting to MongoDB:", err);
-    process.exit(1);
+    await mongoose.connect(process.env.MONGO_URI, {});
+    console.log("Connected to MongoDB successfully!");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1); // Exit process with failure
   }
 };
 
