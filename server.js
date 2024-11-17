518ab5d9bc6f206b3fe3943341705767f2293dd8
Jocephuss <rowlandjosiah14@gmail.com>
2024-11-17 14:46:19 -0700

    ""

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 const questionsRoutes = require("./routes/questionsRoutes");
 require("dotenv").config(); // For environment variables
 
@@ -9,16 +10,19 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 
+// Serve Static Files
+app.use(express.static(path.join(__dirname, "public")));
+
 // Connect to MongoDB
 const db = require("./config/db");
 db.connect();
 
-// Routes
+// API Routes
 app.use("/api/questions", questionsRoutes);
 
-// Default Route
-app.get("/", (req, res) => {
-  res.send("Welcome to the Flashcards API!");
+// Catch-All Route for React/Frontend
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // Start Server
